77a4d512dda9b136bfbb473bffd736d4979f03ed
james howard and tim jung <james@>
2016-06-02 14:24:48 -0700

    Fixed js problems using debugger

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,67 @@
 
 $(document).ready(function() {
-  $("form#profile").submit(function(event) {
-    // useless, maybe later
-    var name = $("input#name").val();
-    var age = parseInt($("input#age").val());
-    var gender = $("select#gender").val();
-    var favcolor = $("input#favcolor").val();
-    // important ones
-    var sign = $("input#sign").val();
-    var pref = $("select#pref").val();
-
-    if (pref === 'male') {
-      if {sign === virgo || cancer} {
-        $(#brad).show();
-      }
-      else if {sign === aries || taurus} {
-        $(#george).show();
-      }
-      else if {sign === capricorn || sagittarius} {
-        $(#taylor).show();
-      }
-      else if {sign === gemini || scorpio} {
-        $(#steve).show();
-      }
-      else if {sign === libra || leo} {
-        $(#louis).show();
-      }
-      else if {sign === auquarius || pisces} {
-        $(#sean).show();
-      }
-    }
-
-    else if (pref === 'female') {
-      if {sign === virgo || cancer} {
-        $(#angelina).show();
-      }
-      else if {sign === aries || taurus} {
-        $(#scarlett).show();
-      }
-      else if {sign === capricorn || sagittarius} {
-        $(#jennifer).show();
-      }
-      else if {sign === gemini || scorpio} {
-        $(#whoopi).show();
-      }
-      else if {sign === libra || leo} {
-        $(#rosie).show();
-      }
-      else if {sign === auquarius || pisces} {
-        $(#daisy).show();
-      }
-    }
-
-
-
-
-var brad = {virgo, cancer}
-
-
-    $("#rate").text(quote);
-    $("#quote").show()
-
+  $("#profile").submit(function(event) {
     event.preventDefault();
+    debugger;
+    // useless, maybe later
+    // var name = $("input#name").val();
+    // var age = parseInt($("input#age").val());
+    var gender = $("#gender").val();
+    // var favcolor = $("input#favcolor").val();
+    // important ones
+    var sign = $("#sign").val();
+    // var pref = $("select#pref").val();
+
+    // if (pref === 'male') {
+      // if ('sign' === 'virgo' || 'cancer') {
+      //   $('#brad').show();
+      // }
+      // else if ('sign' === 'aries' || 'taurus') {
+      //   $('#george').show();
+      // }
+      // else if (sign === 'capricorn' || 'sagittarius') {
+      //   $('#taylor').show();
+      // }
+      // else if (sign === 'gemini' || 'scorpio') {
+      //   $('#steve').show();
+      // }
+      // else if (sign === 'libra' || 'leo') {
+      //   $('#louis').show();
+      // }
+      // else if (sign === 'auquarius' || 'pisces') {
+      //   $('#sean').show();
+      // }
+    // }
+
+    if (gender === 'male') {
+      $('#brad').show();
+    }
+    else if (gender === 'female') {
+      $('#george').show();
+    }
+    else {
+      console.log("wrong");
+    }
+
+    // else if (pref === 'female') {
+      // if {sign === 'virgo' || 'cancer'} {
+      //   $(#angelina).show();
+      // }
+      // else if {sign === 'aries' || 'taurus'} {
+      //   $(#scarlett).show();
+      // }
+      // else if {sign === 'capricorn' || 'sagittarius'} {
+      //   $(#jennifer).show();
+      // }
+      // else if {sign === 'gemini' || 'scorpio'} {
+      //   $(#whoopi).show();
+      // }
+      // else if {sign === 'libra' || 'leo'} {
+      //   $(#rosie).show();
+      // }
+      // else if {sign === 'auquarius' || 'pisces'} {
+      //   $(#daisy).show();
+
+
   });
 });
